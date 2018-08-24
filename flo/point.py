2abbfc7ee9540aad5f548ad3edf9c89c7afb056d
Kevin Kusch <kevinkusch@Kevins-MacBook-Pro.local>
2018-08-24 11:50:20 -0400

    Added some TODOs to point.py

--- flo/point.py
+++ flo/point.py
@@ -9,9 +9,15 @@ class Point(metaclass=MetaFlyweight):
     def __repr__(self):
         return f"Point({self.x}, {self.y})"
 
+    # Do type check here to inform return value
+    # aka for subtracting point - point returns pointdelta
+    # but point - pointdelta returns point...
+
+    # TODO: override THE __add__ and __subtract__ functions!!!
     def get_delta_from(self, other):
         return PointDelta(self.x - other.x, self.y - other.y)
 
+    # TODO: override THE __add__ and __subtract__ functions!!!
     def with_added_delta(self, delta):
         return Point(self.x + delta.x, self.y + delta.y)
 
