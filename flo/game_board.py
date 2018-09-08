92fb6487a73d3be9b089d626a2e91df4ba0e1abc
Kevin Kusch <kevinkusch49@gmail.com>
2018-09-08 10:20:38 -0400

    Refactored GameBoard, removed some unused functions

--- flo/game_board.py
+++ flo/game_board.py
@@ -4,32 +4,25 @@ from abc import ABCMeta, abstractmethod
 
 
 class GameBoard(metaclass=ABCMeta):
-    def __init__(self, points, pairs):
+    def __init__(self, points, pairs, adjacent_point_deltas):
         self.points = points
         self.pairs = pairs
+        self.adjacent_point_deltas = adjacent_point_deltas
+        self._adjacent_points_cache = {}
 
-    # I spend SOOOO much time in this function...
-    # I feel like I could update this as I go...
-    def get_adjacent_points(self, path, ignore=None):
+    def get_adjacent_points(self, path, ignore_points):
         return [[p for p in self.adjacent_points(end_point)
                  if p in self.points and
-                 p not in self._get_off_limit_points(ignore) and
-                 p not in self._get_folding_points(path, end_point)]
+                 p not in self._get_folding_points(path, end_point) and
+                 p not in self._get_off_limit_points(ignore_points)]
             for end_point in path.end_points()
         ]
 
-    def get_random_point(self, ignore=None):
-        non_ignored_points = [p for p in self.points
-                              if p not in self._get_off_limit_points(ignore)]
-        if non_ignored_points:
-            return random.choice(non_ignored_points)
-
-    def get_open_points(self, ignore=None):
+    def get_open_points(self, ignore_points):
         return [p for p in self.points
-                if p not in self._get_off_limit_points(ignore)]
+                if p not in self._get_off_limit_points(ignore_points)]
 
-    def _get_off_limit_points(self, ignore):
-        ignore_points = ignore or []
+    def _get_off_limit_points(self, ignore_points):
         pair_points = [p for pair in self.pairs for p in pair.points]
         return pair_points + ignore_points
 
@@ -39,12 +32,12 @@ class GameBoard(metaclass=ABCMeta):
                 if pt != end_point]
 
     def adjacent_points(self, point):
-        return [point.with_added_delta(d) for d in self.adjacent_point_deltas]
-
-    @property
-    @abstractmethod
-    def adjacent_point_deltas(self):
-        pass
+        if point in self._adjacent_points_cache:
+            return self._adjacent_points_cache[point]
+        else:
+            adj_points = [point + d for d in self.adjacent_point_deltas]
+            self._adjacent_points_cache[point] = adj_points
+            return adj_points
 
     @abstractmethod
     def without_pairs(self, remove_pairs):
@@ -55,22 +48,22 @@ class GridBoard(GameBoard):
     def __init__(self, size, pairs):
         self.size = size
         points = [Point(x, y) for x in range(size) for y in range(size)]
-        super().__init__(points, pairs)
-
-    @property
-    def adjacent_point_deltas(self):
-        return [
+        adjacent_point_deltas = [
             PointDelta(1, 0),
             PointDelta(0, 1),
             PointDelta(-1, 0),
             PointDelta(0, -1),
         ]
+        super().__init__(points, pairs, adjacent_point_deltas)
 
     def without_pairs(self, remove_pairs):
         for pair in remove_pairs:
             if pair not in self.pairs:
                 raise ValueError
 
-        keep_pairs = [pr for pr in self.pairs if pr not in remove_pairs]
+        # Changing this to be mutative to persist the cache
+        # self.pairs = [pr for pr in self.pairs if pr not in remove_pairs]
+        # return self
 
+        keep_pairs = [pr for pr in self.pairs if pr not in remove_pairs]
         return GridBoard(self.size, keep_pairs)
