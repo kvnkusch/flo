496ff63907bb4b68171e96be8f59e98da4d08b42
Kevin Kusch <kevinkusch49@gmail.com>
2018-09-08 10:11:53 -0400

    Flyweight metaclass now manages __init__, is only called once

--- flo/flyweight.py
+++ flo/flyweight.py
@@ -3,7 +3,17 @@ class MetaFlyweight(type):
         super().__init__(name, bases, dct)
         cls._instances = dict()
         cls.__new__ = MetaFlyweight._get_instance
+        cls._init = cls.__init__
+        cls.__init__ = lambda *args: None
 
     def _get_instance(cls, *args, **kwargs):
-        return cls._instances.setdefault(
-            (args, tuple(kwargs.items())), object.__new__(cls))
+        key = (args, tuple(kwargs.items()))
+        if key in cls._instances:
+            return cls._instances[key]
+        else:
+            obj = object.__new__(cls)
+            cls._init(obj, *args, **kwargs)
+            cls._instances[key] = obj
+            return obj
+        # return cls._instances.setdefault(
+        #     (args, tuple(kwargs.items())), object.__new__(cls))
